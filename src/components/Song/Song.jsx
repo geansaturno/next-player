68db7b5463ee9736fdb86e23a45fc31f0441a1d7
Gean Saturno <geangoncalves.gst@ciclic.com.br>
2023-09-14 22:38:40 -0300

    style: song style

--- src/components/Song/Song.jsx
+++ src/components/Song/Song.jsx
@@ -1,9 +1,50 @@
-import style from './Song.module.scss'
+"use client";
+
+import Image from "next/image";
+
+import style from "./Song.module.scss";
+import { useEffect } from "react";
+import { Play } from "../../icons/Play";
 
 export default function Song(song) {
-    return (
-        <section className={`${style.section} container`}>
-            <p>oi</p>
-        </section>
-    )
-} 
+  let isPlaying = false;
+  let audio;
+
+  function tooglePlay() {
+    isPlaying ? audio.pause() : audio.play();
+  }
+
+  useEffect(() => {
+    audio = new Audio(`/assets/audio/${song.song.files.audio}`);
+
+    if (audio) {
+      audio.onplay = () => (isPlaying = true);
+      audio.onpause = () => (isPlaying = false);
+    }
+  });
+
+  return (
+    <section className={`${style.section} container`}>
+      <Image
+        className={style.image}
+        src={`/assets/images/${song.song.files.coverArt}`}
+        width={204}
+        height={204}
+        alt={`${song.song.title} - ${song.song.artist}`}
+      />
+      <div className={style.data}>
+        <p className={style.title}>{song.song.title}</p>
+
+        <p className={style.desc}>
+          {`${song.song.artist} | ${song.song.album.title} | ${song.song.album.year}`}
+        </p>
+
+        <button onClick={tooglePlay} className={style.button}>
+          <Play className={style.icon} />
+        </button>
+      </div>
+
+      <p>Other albums</p>
+    </section>
+  );
+}
---
+++ src/icons/Play.jsx
@@ -0,0 +1,5 @@
+export function Play({className}) {
+    return (
+        <svg className={className} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 32 32"><path d="M26 14.845a1.333 1.333 0 0 1 0 2.31l-16 9.237c-.889.514-2-.128-2-1.154V6.762a1.333 1.333 0 0 1 2-1.154l16 9.237Z"/></svg>
+    )
+}
